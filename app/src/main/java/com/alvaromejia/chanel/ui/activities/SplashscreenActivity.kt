7256df7fda97c98d24afeeb324d710f80cca662c
Juan Mejía <juanes.reich@gmail.com>
2021-07-21 22:20:49 -0500

    Initial commit

---
+++ app/src/main/java/com/alvaromejia/chanel/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.alvaromejia.chanel.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alvaromejia.chanel.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
