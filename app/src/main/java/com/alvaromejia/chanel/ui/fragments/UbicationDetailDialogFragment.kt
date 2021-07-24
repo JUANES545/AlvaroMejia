4ad889b5dca17d668f698b62f8543effab095cda
Juan Mejía <juanes.reich@gmail.com>
2021-07-24 01:53:18 -0500

    Adding new activities and animations

--- app/src/main/java/com/alvaromejia/chanel/ui/fragments/UbicationDetailDialogFragment.kt
+++ app/src/main/java/com/alvaromejia/chanel/ui/fragments/UbicationDetailDialogFragment.kt
@@ -35,7 +35,7 @@ class UbicationDetailDialogFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ubication_detail_dialog, container, false)
+        return inflater.inflate(R.layout.fragment_location_detail_dialog, container, false)
     }
 
     companion object {
--- app/src/main/java/com/alvaromejia/chanel/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/alvaromejia/chanel/ui/fragments/UbicationFragment.kt
@@ -35,7 +35,7 @@ class UbicationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ubication, container, false)
+        return inflater.inflate(R.layout.fragment_location, container, false)
     }
 
     companion object {
