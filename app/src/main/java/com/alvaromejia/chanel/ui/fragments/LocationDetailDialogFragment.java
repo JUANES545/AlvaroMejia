f799bc55f84fcdf9ba5b33fac73299ce95867e59
Juan Mejía <juanes.reich@gmail.com>
2021-07-28 18:51:48 -0500

    Creating default navigator host

---
+++ app/src/main/java/com/alvaromejia/chanel/ui/fragments/LocationDetailDialogFragment.java
@@ -0,0 +1,6 @@
+package com.alvaromejia.chanel.ui.fragments;
+
+import android.app.Fragment;
+
+public class LocationDetailDialogFragment extends Fragment {
+}
---
+++ app/src/main/java/com/alvaromejia/chanel/ui/fragments/LocationFragment.java
@@ -0,0 +1,6 @@
+package com.alvaromejia.chanel.ui.fragments;
+
+import android.app.Activity;
+
+public class LocationFragment extends Activity {
+}
